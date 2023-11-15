25ba5873ea50c0c05533a43b6ded4a73daad914a
lward27 <lward@ippon.fr>
2023-11-15 14:23:38 -0500

    initial commit

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='finance_app_database_service',
+      version='1.0',
+      description='Talks to the Database Man',
+      author='Lucas Ward',
+      author_email='lward@ipponusa.com',
+      url='https://us.ippon.tech/',
+      packages=find_packages(),
+     )
